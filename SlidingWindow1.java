4eb233effbcb49fdba1ddcb92f7f4cb9a29eeef9
Justin Stransky <dva@colostate.edu>
2026-01-09 09:04:44 -0500

    Best Time to Buy and Sell Stock

---
+++ SlidingWindow1.java
@@ -0,0 +1,15 @@
+class SlidingWindow1 {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int lowestBuyPrice = prices[0];
+        for (int ii = 1; ii < prices.length; ii++) {
+            if (prices[ii] < lowestBuyPrice) {
+                lowestBuyPrice = prices[ii];
+            } else {
+                int tempProfit = prices[ii] - lowestBuyPrice;
+                maxProfit = Math.max(tempProfit, maxProfit);
+            }
+        }
+        return maxProfit;
+    }
+}
